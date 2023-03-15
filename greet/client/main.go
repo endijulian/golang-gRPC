66705dc32b614901a9d53c2cae25bbbc834b7bfe
endijulian <endijulian080798@gmail.com>
2023-03-15 17:46:33 +0700

    modified file

--- greet/client/main.go
+++ greet/client/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	pb "github.com/Clement-Jean/grpc-go-course/greet/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -17,4 +18,8 @@ func main() {
 	}
 
 	defer conn.Close()
+
+	c := pb.NewGreetServiceClient(conn)
+
+	doGreet(c)
 }
